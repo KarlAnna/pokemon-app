36c487c1287feaeddb5e215825f5a081e4fde543
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2023-02-27 13:21:49 +0200

    fix?

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,6 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
-git push -f git@github.com:karlAnna/pokemon-app.git main:gh-pages
+git push -f git@github.com:KarlAnna/pokemon-app.git main:gh-pages
 
 cd -
